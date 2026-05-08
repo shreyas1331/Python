e48008e8a16397525b9aa85081f5025ff12f8e72
Shreyas Mahajan <shreyasmahajan@Shreyass-MacBook-Air.local>
2026-05-08 11:55:35 +0530

    bank account example added

--- CoreConcepts10(OOPS)/01_class.py
+++ CoreConcepts10(OOPS)/01_class.py
@@ -9,3 +9,25 @@ print(harry.name,harry.salary)
 rohan=Employee()
 rohan.name="rohannnnn"
 print(rohan.name,rohan.salary)
+
+
+class BankAccount:
+    def __init__(self,owner,balance=0):
+        self.owner=owner
+        self.balance=balance
+
+    def deposit(self,amount):
+        self.balance+=amount
+        print(f"u have deposited {amount}, and the current balance is {self.balance}")
+
+    def withdraw(self,amount):
+        self.balance-=amount
+        print(f"u have withdrawn {amount}, and the current balance is {self.balance}")
+
+    def totalBal(self):
+        print(f"ur total balance is {self.balance}")
+
+shreyasAcc=BankAccount("shreyas",3000)
+shreyasAcc.deposit(1000)
+shreyasAcc.totalBal()
+shreyasAcc.withdraw(1500)
